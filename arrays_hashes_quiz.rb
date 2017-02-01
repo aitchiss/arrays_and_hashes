3efd2e3719fe185a82371e1693f047c71fa6b6c2
Suzanne <suzanne.aitchison@gmail.com>
2017-02-01 17:00:54 +0000

    Answers to all questions added

--- arrays_hashes_quiz.rb
+++ arrays_hashes_quiz.rb
@@ -3,23 +3,40 @@
 lines = ['Gyle Centre', 'Edinburgh Park', 'Murrayfield Stadium', 'Haymarket', 'Princes Street']
 
 # 1. Work out how many stops there are in the array
-# puts lines.length()
+puts lines.length()
 
 # 2. Return 'Edinburgh Park' from the array
+puts lines[1]
 
 # 3. How many ways can we return 'Princes Street' from the array?
+puts lines.last
+puts lines[4]
+puts lines [-1]
 
 # 4. Work out the index position of 'Haymarket'
 
+puts lines.index("Haymarket")
+
 # 5. Add 'Airport' to the start of the array
+
+lines.unshift("Airport")
 
 # 6. Add 'York Place' to the end of the array
 
+lines << "York Place"
+
 # 7. Remove 'Edinburgh Park' from the array using it's name
+
+lines.delete("Edinburgh Park")
 
 # 8. Delete 'Edinburgh Park' from the array by index
 
+lines.delete_at(2)
+
+
 # 9. Reverse the positions of the stops in the array
+
+lines.reverse!
 
 ### B. Given the following data structure:
 
@@ -27,13 +44,23 @@ my_hash = {"0" => "Zero", 1 => "One", :two => "Two", "two" => 2}
 
 # 1. How would you return the string `"One"`?
 
+puts my_hash[1]
+
 # 2. How would you return the string `"Two"`?
+
+puts my_hash[:two]
 
 # 3. How would you return the number `2`?
 
+puts my_hash["two"]
+
 # 4. How would you add `{3 => "Three"}` to the hash?
 
+my_hash[3] = "Three"
+
 # 5. How would you add `{:four => 4}` to the hash?
+
+my_hash[:four] = 4
 
 ### C. Given the following data structure:
 
@@ -71,18 +98,46 @@ users = {
 
 # 1. Return Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
 
+puts users["Jonathan"][:twitter]
+
 # 2. Return Erik's hometown
+
+puts users["Erik"][:home_town]
 
 # 3. Return the array of Erik's favorite numbers
 
+puts users["Erik"][:favourite_numbers]
+
 # 4. Return the type of Avril's pet Colin
+
+puts users["Avril"][:pets]["colin"]
 
 # 5. Return the smallest of Erik's favorite numbers
 
+puts users["Erik"][:favourite_numbers].sort.first
+
 # 6. Add the number `7` to Erik's favorite numbers
+
+users["Erik"][:favourite_numbers] << 7
 
 # 7. Change Erik's hometown to Edinburgh
 
+users["Erik"][:home_town] = "Edinburgh"
+
 # 8. Add a pet dog to Erik called "Fluffy"
 
+users["Erik"][:pets]["Fluffy"] = :dog
+
 # 9. Add yourself to the users hash
+
+users["Suzanne"] = {
+  twitter: "s_aitchison",
+  favourite_numbers: [5, 4, 3, 2, 1],
+  home_town: "Alloa",
+  pets: {
+    "Barry" => :imaginary_dog,
+    "Elspeth" => :imaginary_cat,
+    "Poseidon" => :dead_goldfish
+  }
+
+}
